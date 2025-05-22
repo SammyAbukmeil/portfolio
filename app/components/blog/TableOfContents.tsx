d626116f3a06eb360015abb2ed6d950146a3c994
Sammy Abukmeil <sammyabukmeil@gmail.com>
2025-05-22 14:14:08 +0100

    Finish search algorithms

--- app/components/blog/TableOfContents.tsx
+++ app/components/blog/TableOfContents.tsx
@@ -4,15 +4,15 @@ interface Props {
   items: { text: string; link: string; nested: boolean }[];
 }
 
-export default function Navbar({ items }: Props) {
+export default function TableOfContents({ items }: Props) {
   return (
     <section className="mt-8 bg-zinc-100 dark:bg-primary-dark p-4">
-      <p>Contents</p>
+      <p>Table of Contents</p>
       <ul className="list-none">
         {items.map((item, i) => (
           <li
             key={i}
-            className={`flex items-center ${item.nested ? "ml-4" : ""}`}
+            className={`flex items-center my-0.5 ${item.nested ? "ml-4" : ""}`}
           >
             <Hash size={18} />
             <a className="underline" href={item.link}>
