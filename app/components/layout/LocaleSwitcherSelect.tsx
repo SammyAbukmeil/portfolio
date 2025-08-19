4483df6d7985066d4ff812ed6fe2260b35149f3e
Sammy Abukmeil <sammyabukmeil@gmail.com>
2025-08-19 11:12:05 +0100

    Add closing paragraphs to blog posts

--- app/components/layout/LocaleSwitcherSelect.tsx
+++ app/components/layout/LocaleSwitcherSelect.tsx
@@ -1,9 +1,10 @@
-'use client';
+"use client";
 
-import { useParams } from 'next/navigation';
-import { Locale } from 'next-intl';
-import { ChangeEvent, ReactNode, useTransition } from 'react';
-import { usePathname, useRouter } from '@/i18n/navigation';
+import { useParams } from "next/navigation";
+import { Locale } from "next-intl";
+import { ChangeEvent, ReactNode, useTransition } from "react";
+import { usePathname, useRouter } from "@/i18n/navigation";
+import { ChevronDown, Languages } from "lucide-react";
 
 type Props = {
   children: ReactNode;
@@ -14,7 +15,7 @@ type Props = {
 export default function LocaleSwitcherSelect({
   children,
   defaultValue,
-  label
+  label,
 }: Props) {
   const router = useRouter();
   const [isPending, startTransition] = useTransition();
@@ -28,28 +29,29 @@ export default function LocaleSwitcherSelect({
         // @ts-expect-error -- TypeScript will validate that only known `params`
         // are used in combination with a given `pathname`. Since the two will
         // always match for the current route, we can skip runtime checks.
-        {pathname, params},
-        {locale: nextLocale}
+        { pathname, params },
+        { locale: nextLocale }
       );
     });
   }
 
   return (
     <label
-      className={`relative ${
-        isPending ? 'transition-opacity opacity-30' : ''
+      className={`flex mr-2 relative ${
+        isPending ? "transition-opacity opacity-30" : ""
       }`}
     >
       <p className="sr-only">{label}</p>
+      <Languages />
       <select
-        className="inline-flex appearance-none bg-transparent pl-2 pr-6 focus:outline-none"
+        className="inline-flex appearance-none bg-transparent pl-2 pr-1 focus:outline-none"
         defaultValue={defaultValue}
         disabled={isPending}
         onChange={onSelectChange}
       >
         {children}
       </select>
-      <span className="pointer-events-none absolute right-1 bottom-[3px] text-md">⌄</span>
+      <ChevronDown />
     </label>
   );
 }
