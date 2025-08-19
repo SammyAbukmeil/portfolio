f93e4e2edca13a7499d45d44a06bd5e448d884e2
Sammy Abukmeil <sammyabukmeil@gmail.com>
2025-08-19 16:06:03 +0100

    Fix links after changing language

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,5 +1,5 @@
 import React, { ComponentPropsWithoutRef } from "react";
-import Link from "next/link";
+import { Link } from "@/i18n/navigation";
 import { highlight } from "sugar-high";
 
 type HeadingProps = ComponentPropsWithoutRef<"h1">;
